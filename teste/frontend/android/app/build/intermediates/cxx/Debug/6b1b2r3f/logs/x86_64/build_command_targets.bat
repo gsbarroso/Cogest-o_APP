3cd40a964fabdbc50b3a7b5e77269b4e0d69497e
Gustavo Barroso <gustavo2804sb@gmail.com>
2025-08-12 20:23:11 -0300

    Ignorar arquivos de build

--- teste/frontend/android/app/build/intermediates/cxx/Debug/6b1b2r3f/logs/x86_64/build_command_targets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\\Users\\gusta\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\CPEjr\\Cogestao_APP\\teste\\frontend\\android\\app\\.cxx\\Debug\\6b1b2r3f\\x86_64" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
